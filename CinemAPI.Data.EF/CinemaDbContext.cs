a6e85dcc924b0b9dc6b131908f3e02d6f71fe215
Teodor Hanev <t.hanev@ultraplay.co>
2018-06-22 18:16:18 +0300

    Added DbContext

---
+++ CinemAPI.Data.EF/CinemaDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CinemAPI.Models;
+using System.Data.Entity;
+
+namespace CinemAPI.Data.EF
+{
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext()
+            : base("CinemaDbContext")
+        {
+        }
+
+        public virtual IDbSet<Cinema> Cinemas { get; set; }
+
+        public virtual IDbSet<Room> Rooms { get; set; }
+
+        public virtual IDbSet<Movie> Movies { get; set; }
+
+        public virtual IDbSet<Projection> Projections { get; set; }
+    }
+}
