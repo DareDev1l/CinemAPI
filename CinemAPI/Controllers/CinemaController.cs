3037e08a29e248143e29a9d6b217a23a3ebba115
Teodor Hanev <t.hanev@ultraplay.co>
2018-06-22 12:39:34 +0300

    Added Create endpoint method for cinemas

---
+++ CinemAPI/Controllers/CinemaController.cs
@@ -0,0 +1,31 @@
+﻿using CinemAPI.Data;
+using CinemAPI.Models;
+using CinemAPI.Models.Input.Cinema;
+using System.Web.Http;
+
+namespace CinemAPI.Controllers
+{
+    public class CinemaController : ApiController
+    {
+        private readonly ICinemaRepository cinemaRepo;
+
+        public CinemaController(ICinemaRepository cinemaRepo)
+        {
+            this.cinemaRepo = cinemaRepo;
+        }
+
+        public IHttpActionResult Create(CinemaCreationModel model)
+        {
+            Cinema cinema = cinemaRepo.GetByNameAndAddress(model.Name, model.Address);
+
+            if (cinema == null)
+            {
+                cinemaRepo.Insert(cinema);
+
+                return Ok();
+            }
+
+            return BadRequest("Cinema already exists");
+        }
+    }
+}
